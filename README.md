a650a5aa9782b03f5678f41b564db2ed2b694382
MuchaiSr <150523983+MuchaiSr@users.noreply.github.com>
2025-04-24 14:33:10 +0100

    Implement an event listener and update README file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# etch-a-sketch
+# Etch-a-sketch
+This is a project that follows The Odin Project. Learners are tasked with creating building a browser version of something between a sketchpad and an Etch-A-Sketch. 
+The project was meant to test the learners understanding of DOM manipulation as well as revise some CSS concepts such as flexbox.
--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -27,5 +27,84 @@
     divList.forEach((div) => {
         section.appendChild(div);
         div.style.border = "solid 1px black";
+        div.addEventListener("mouseover", () => {
+            div.classList.add("hovered");
+          });
     });
+
+    const promptButton = document.createElement("button");
+    promptButton.textContent = "Press me!";
+    document.body.appendChild(promptButton);
+    document.body.appendChild(section);
+
+    promptButton.addEventListener("click", () => {
+        const promptAnswer = prompt("What number of squares per side would you like for a new grid?", 0);
+        const numValue = Number(promptAnswer);
+        const divList = [];
+
+
+        if (isNaN(numValue) & promptAnswer !== null){
+            alert ("Invalid! Please enter a number.");
+            prompt ("What number of squares per side would you like for a new grid?", 0);
+        } else if (numValue < 100) {
+            function removeDivs() {
+                divList.forEach((div) => {
+                    section.removeChild(div); // Notice the way the child nodes are removed. 
+                    // parent.remove(child) does not exist, but parent.removeChild(child) does.
+                });
+                divList.length = 0; // Notice how the array is cleared
+            }
+
+            removeDivs();
+
+            const divsAcross = numValue;
+            const divsDown = numValue;
+
+            const totalDivs = divsAcross * divsDown;
+
+            for (let i = 0; i < totalDivs; i++) {
+                const div = document.createElement("div");
+                div.setAttribute("class", "child");
+
+                divList.push(div);  
+            }
+
+            divList.forEach((div) => {
+                section.appendChild(div);
+                div.style.border = "solid 1px black";
+                div.addEventListener("mouseover", () => {
+                    div.classList.add("hovered");
+                  });
+            });
+
+            localStorage.setItem("gridSettings", numValue);
+
+        } else if (numValue > 100) {
+            alert ("ERROR!");
+        }
+    });
+
+    const savedValue = localStorage.getItem("gridSettings");
+
+    if (savedValue !== null) {
+        const divsAcross = savedValue;
+        const divsDown = savedValue;
+
+        const totalDivs = divsAcross * divsDown;
+
+        for (let i = 0; i < totalDivs; i++) {
+            const div = document.createElement("div");
+            div.setAttribute("class", "child");
+
+            divList.push(div);  
+        }
+
+        divList.forEach((div) => {
+            section.appendChild(div);
+            div.style.border = "solid 1px black";
+            div.addEventListener("mouseover", () => {
+                div.classList.add("hovered");
+              });
+        });
+    }
 })();
